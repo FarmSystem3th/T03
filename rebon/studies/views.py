31ee6cf37c2656b8a7f9f838dd04872cf6dca329
trsf0 <sming1611@naver.com>
2024-09-08 16:28:36 +0900

    Studies, Works search

--- rebon/studies/views.py
+++ rebon/studies/views.py
@@ -6,6 +6,8 @@ from urllib.request import urlopen
 class StudyListView(APIView):
 
     def get(self, request):
+        course_nm_query = request.query_params.get('course_nm', None)
+
         url = "http://openapi.seoul.go.kr:8088/4d736965656d696e3836714f616358/xml/OfflineCourse/1/100"
         response = urlopen(url)
         xml_data = response.read()
@@ -21,13 +23,14 @@ class StudyListView(APIView):
             course_end_dt = row.find('COURSE_END_DT').text
             course_apply_url = row.find('COURSE_APPLY_URL').text
 
-            required_data.append({
-                'course_nm': course_nm,
-                'course_request_str_dt': course_request_str_dt,
-                'course_request_end_dt': course_request_end_dt,
-                'course_str_dt': course_str_dt,
-                'course_end_dt': course_end_dt,
-                'course_apply_url': course_apply_url
-            })
+            if course_nm_query is None or course_nm_query.lower() in course_nm.lower():
+                required_data.append({
+                    'course_nm': course_nm,
+                    'course_request_str_dt': course_request_str_dt,
+                    'course_request_end_dt': course_request_end_dt,
+                    'course_str_dt': course_str_dt,
+                    'course_end_dt': course_end_dt,
+                    'course_apply_url': course_apply_url
+                })
 
         return Response(required_data)
--- rebon/works/views.py
+++ rebon/works/views.py
@@ -6,6 +6,8 @@ from urllib.request import urlopen
 class WorkListView(APIView):
 
     def get(self, request):
+        recrt_title_query = request.query_params.get('recrtTitle', None)
+
         url = "http://apis.data.go.kr/B552474/SenuriService/getJobList?ServiceKey=pXFmS50aPfKUlYjsdr9%2Flv3qzXHHI6vphqfxKoa2uyTnmaMuPFrVX94VJgKRCvvVVwmtKtPI%2BhtnyMEk1VgXRg%3D%3D&numOfRows=20&pageNo=1"
         response = urlopen(url)
         xml_data = response.read()
@@ -21,11 +23,12 @@ class WorkListView(APIView):
             fr_dd = item.find('frDd').text
             to_dd = item.find('toDd').text
 
-            required_data.append({
-                'recrtTitle': recrt_title,
-                'frDd': fr_dd,
-                'toDd': to_dd,
-                'deadline': deadline,
-            })
+            if recrt_title_query is None or recrt_title_query.lower() in recrt_title.lower():
+                required_data.append({
+                    'recrtTitle': recrt_title,
+                    'frDd': fr_dd,
+                    'toDd': to_dd,
+                    'deadline': deadline,
+                })
 
         return Response(required_data)
